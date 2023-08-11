07c877470c3038cac89ecf945bd4ba2b01342c04
GAURAV SHARMA <gsharma1154@gmail.com>
2023-08-11 23:42:15 +0530

    pattern 3,4,5

---
+++ src/Patterns/Q5.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class Q5 {
+    public static void main(String[] args) {
+        for (int i = 5; i >0 ; i--) {
+            for (int j=1;j<=i ; j++) {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
