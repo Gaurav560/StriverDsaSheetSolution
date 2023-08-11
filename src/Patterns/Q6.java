894f6a383e003ded9456e8489e169190538b1110
GAURAV SHARMA <gsharma1154@gmail.com>
2023-08-11 23:44:05 +0530

    pattern 6

---
+++ src/Patterns/Q6.java
@@ -0,0 +1,13 @@
+package Patterns;
+
+public class Q6 {
+    public static void main(String[] args) {
+        for (int i = 5; i >0 ; i--) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
